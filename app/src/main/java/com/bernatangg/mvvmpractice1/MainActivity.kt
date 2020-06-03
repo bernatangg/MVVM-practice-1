9a5c94a7bea411ef737543cd7b3dd2b384de0ea7
Anggie <djavasoegar07@gmail.com>
2020-06-04 05:59:49 +0700

    initial commit

---
+++ app/src/main/java/com/bernatangg/mvvmpractice1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bernatangg.mvvmpractice1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
